fe7ac2f7d61678a33afdc71849f5b0d0fb319032
edlu2100 <edwinlundback@gmail.com>
2023-09-04 17:21:44 +0200

    tailwind done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                "footergray": "rgb(56, 56, 56)"
+            }
+
+        },
+    },
+    plugins: [],
+}
